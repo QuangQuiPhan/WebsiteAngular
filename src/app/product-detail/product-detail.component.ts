a0431ef1ccb0cc4f8e3b0c1a67ddf0e1f92f6ca8
QuangQuiPhan <pquangqui.it@gmail.com>
2022-05-16 16:30:01 +0700

    v7.0

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -12,18 +12,21 @@ import { ShoppingCartService } from '../shopping-cart.service';
 })
 
 export class ProductDetailComponent implements OnInit {
-  product: ProductsModules|undefined;
+  product?: ProductsModules;
   constructor(private productService:ProductServiceService, private route:ActivatedRoute, private cartService: ShoppingCartService) { }
-
-  i: number = this.cartService.countItems();
-  ngOnInit(): void {
-    const id = +this.route.snapshot.params['id'];
-    this.product = this.productService.getProduct(id);
-  }
   
+  quantity: number|undefined = 1;
+  price: number|undefined = 1;
   addItem(item: any){
     this.cartService.addToCart(item);
     window.alert('Thêm vào giỏ hàng thành công!!!');
+  }
+  ngOnInit(): void {
+    const id = +this.route.snapshot.params['id'];
+    this.product = this.productService.getProduct(id);
+
+    this.quantity = this.product?.quantity;
+    this.price = this.product?.price;
   }
   
 }
